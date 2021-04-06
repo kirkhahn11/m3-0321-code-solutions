6eaa0f57f835993f2324de590760d8d63c258756
Kirk Hahn <kirkhahn11@yahoo.com>
2021-04-06 23:50:10 +0000

    Completed fetch in react practice

---
+++ fetch-in-react-practice/client/components/app.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import PageTitle from './page-title';
+import TodoList from './todo-list';
+import TodoForm from './todo-form';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.toggleCompleted = this.toggleCompleted.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllTodos();
+  }
+
+  getAllTodos() {
+    fetch('/api/todos')
+      .then(res => res.json())
+      .then(data => this.setState({ todos: data }))
+      .catch(err => console.error(err));
+  }
+
+  addTodo(newTodo) {
+    fetch('/api/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newTodo)
+    })
+      .then(res => res.json())
+      .then(this.state.todos.push(newTodo.task));
+    this.getAllTodos();
+  }
+
+  toggleCompleted(todoId) {
+    const newTodos = this.state.todos;
+    newTodos[todoId - 1].isCompleted = !newTodos[todoId - 1].isCompleted;
+
+    fetch(`/api/todos/${todoId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        isCompleted: newTodos[todoId - 1].isCompleted
+      })
+    })
+      .then(res => res.json())
+      .then(data => this.setState({ todos: newTodos }));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col pt-5">
+            <PageTitle text="Todo App"/>
+            <TodoForm onSubmit={this.addTodo}/>
+            <TodoList todos={this.state.todos} toggleCompleted={this.toggleCompleted}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
